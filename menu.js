c388e25f9f9418464efc9d2d4204e29aa2bfaaae
Nahuel Avila <avilanahuel99@gmail.com>
2023-04-22 16:16:57 -0300

    3.0 Diseño shop terminado

--- menu.js
+++ menu.js
@@ -4,3 +4,20 @@ const lista = document.getElementById("lista");
 menu.addEventListener("click", () => {
   lista.classList.toggle("menu-show");
 });
+
+// ----------------------dropdown-----------------------
+
+let filtroboton = document.querySelectorAll(".menu-texto");
+
+filtroboton.forEach(boton => {
+  boton.addEventListener("click", () => {
+    
+    let menu = boton.nextElementSibling;
+    console.log(menu);
+    let height = 0;
+    if (menu.clientHeight == "0") {
+      height = menu.scrollHeight;
+    }
+    menu.style.height = `${height}px`;
+  });
+});
